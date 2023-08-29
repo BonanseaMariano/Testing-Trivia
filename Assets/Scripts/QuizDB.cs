50fc2b6125f185b9a93784ec84e37f10b7ec5fbd
Mariano Bonansea <marianobonanseapetrovial@gmail.com>
2023-08-28 22:42:38 -0300

    DB

---
+++ Assets/Scripts/QuizDB.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizDB : MonoBehaviour
+{
+    [SerializeField] private List<Question> m_questionList = null; 
+}
